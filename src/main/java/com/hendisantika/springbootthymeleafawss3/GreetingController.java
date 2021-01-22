c0322d3c909ffaf65b0df34cef2cf67ca2a45388
Hendi Santika <hendisantika@gmail.com>
2021-01-23 06:03:33 +0700

    #8 - Add uploadS3 in GreetingController

--- src/main/java/com/hendisantika/springbootthymeleafawss3/GreetingController.java
+++ src/main/java/com/hendisantika/springbootthymeleafawss3/GreetingController.java
@@ -1,10 +1,22 @@
 package com.hendisantika.springbootthymeleafawss3;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -43,6 +55,38 @@ public class GreetingController {
     public ModelAndView renderUploadPage() {
         //System.out.println(accesskey  + bucketName + secretkey);
         return new ModelAndView("uploadFiles");
+    }
+
+    @PostMapping(value = "/upload")
+    public ModelAndView uploadS3(@RequestParam("photo") MultipartFile image, @RequestParam(name = "desc") String desc) {
+        ModelAndView returnPage = new ModelAndView();
+        System.out.println("description      " + desc);
+        System.out.println(image.getOriginalFilename());
+
+        BasicAWSCredentials cred = new BasicAWSCredentials(accesskey, secretkey);
+        // AmazonS3Client client=AmazonS3ClientBuilder.standard().withCredentials(new
+        // AWSCredentialsProvider(cred)).with
+        AmazonS3 client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(cred))
+                .withRegion(Regions.US_EAST_1).build();
+        try {
+            PutObjectRequest put = new PutObjectRequest(bucketName, image.getOriginalFilename(),
+                    image.getInputStream(), new ObjectMetadata()).withCannedAcl(CannedAccessControlList.PublicRead);
+            client.putObject(put);
+
+            String imgSrc = "http://" + bucketName + ".s3.amazonaws.com/" + image.getOriginalFilename();
+
+            returnPage.setViewName("showImage");
+            returnPage.addObject("name", desc);
+            returnPage.addObject("imgSrc", imgSrc);
+
+            //Save this in the DB.
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            returnPage.setViewName("error");
+        }
+        return returnPage;
+
     }
 
 }
